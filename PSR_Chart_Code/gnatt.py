54f233e5b227d859335a0d6590cc2033e5233533
Amber Parker <amberparker@arizona.edu>
2024-11-06 11:33:58 -0700

    updated data

--- PSR_Chart_Code/gnatt.py
+++ PSR_Chart_Code/gnatt.py
@@ -10,7 +10,7 @@ from datetime import datetime
 script_dir = os.path.dirname(os.path.abspath(__file__))
 
 # Define the relative path to your TSV file
-file_path = os.path.join(script_dir, '25014 - Camera for Aerospace Situational Awareness - Team Tasks 10_15_24.tsv')
+file_path = os.path.join(script_dir, '25014 - Camera for Aerospace Situational Awareness - Team Tasks 11_5_24.tsv')
 
 # Load the TSV file into a DataFrame
 df = pd.read_csv(file_path, sep='\t')
